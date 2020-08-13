9bd7ceffe92525efe7003ebc2063ff38dbb52e0d
Xu <50079470+aeroxu@users.noreply.github.com>
2020-08-13 10:48:54 -0400

    Updates

---
+++ precache-manifest.27d36e87a34c85c4dc9f67679e441c10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2681dc02d8403656618d943329c73ce5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4933b42ab41057100167",
+    "url": "/monster-rolodex/static/css/main.79ae57a8.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4933b42ab41057100167",
+    "url": "/monster-rolodex/static/js/main.cb9b3d43.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
